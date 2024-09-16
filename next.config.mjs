a72baee608d209095eea45581d7d8910294ac43e
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-09-16 14:52:28 +0700

    refactor: Update next.config.mjs and AppShell component
    - Update next.config.mjs to include port and pathname for remote image patterns
    - Update AppShell component to dynamically import Navbar component and apply Roboto font

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'static.nike.com' },
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'static.nike.com', port: '', pathname: '/**' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com', port: '', pathname: '/**' },
     ],
   },
 };
--- src/components/layouts/AppShell/index.tsx
+++ src/components/layouts/AppShell/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import Navbar from '../Navbar';
+
 import { useRouter } from 'next/router';
+import { Roboto } from 'next/font/google';
+import dynamic from 'next/dynamic';
+
+const Navbar = dynamic(() => import('../Navbar'), {
+  loading: () => <p>loading...</p>,
+});
+
+const roboto = Roboto({ subsets: ['latin'], weight: '400' });
 
 const AppShell = ({ children }: { children: React.ReactNode }) => {
   const { pathname } = useRouter();
+
   const disableNav = ['/auth/login', '/auth/register', '/404'];
   return (
-    <main>
+    <main className={roboto.className}>
       {!disableNav.includes(pathname) && <Navbar />}
       <div className="container flex flex-col justify-center">{children}</div>
     </main>
